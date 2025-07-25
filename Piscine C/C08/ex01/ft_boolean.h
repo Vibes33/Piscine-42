20c8b9cfacd0a63e052fbb25c3d2021639115200
Ryan <tentyrisd@gmail.com>
2025-07-25 11:06:40 +0200

    C08 add

---
+++ Piscine C/C08/ex01/ft_boolean.h
@@ -0,0 +1,17 @@
+#ifdef FT_BOOLEAN_H
+#define FT_BOOLEAN_H
+
+#include <unistd.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define EVEN_MSG "I have an even number of arguments."
+#define ODD_MSG "I have an odd number of arguments."
+
+typedef int t_bool;
+
+#define EVEN(nbr) (nbr % 2 == 0)
+#define SUCCESS 0
+
+#endif //FT_BOOLEAN_H
